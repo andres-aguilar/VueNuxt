1f72c7bbd7b6233b3b8fa1c9098dfb9f748fd5c9
Andrés Aguilar <andresyoshimar@gmail.com>
2018-10-24 14:32:09 -0500

    Agregando la carpeta dist para gh-pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,11 @@
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+    router: {
+      base: '/VueNuxt/'
+    }
+  } : {}
+
 export default {
+    ...routerBase,
     css: [
         'bulma/css/bulma.css',
         '~/assets/style.css'
